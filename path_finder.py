5e3a2c2ac9d543319ea9b9f790a768ccfd2e36a1
Devanshi Patel <55372538+Devanshi512@users.noreply.github.com>
2020-10-15 17:54:23 -0400

    Added "Cell" class

--- path_finder.py
+++ path_finder.py
@@ -14,3 +14,60 @@ WHITE = (255, 255, 255)  # Walkable Node
 BLACK = (0, 0, 0)  # Obstacle
 PURPLE = (128, 0, 128)  # Final Path
 GREY = (128, 128, 128)
+
+
+class Cell:
+	def __init__(self, row, col, width, total_rows):
+		self.row = row
+		self.col = col
+		self.x = row * width
+		self.y = col * width
+		self.color = WHITE
+		self.neighbors = []
+		self.width = width
+		self.total_rows = total_rows
+
+	def is_closed(self):
+		return self.color == RED
+
+	def is_open(self):
+		return self.color == GREEN
+
+	def is_obstacle(self):
+		return self.color == BLACK
+
+	def is_start(self):
+		return self.color == ORANGE
+
+	def is_end(self):
+		return self.color == TURQUOISE
+
+	def make_start(self):
+		self.color = ORANGE
+
+	def make_closed(self):
+		self.color = RED
+
+	def make_open(self):
+		self.color = GREEN
+
+	def make_obstacle(self):
+		self.color = BLACK
+
+	def make_end(self):
+		self.color = TURQUOISE
+
+	def make_path(self):
+		self.color = PURPLE
+
+	def reset(self):
+		self.color = WHITE
+
+	def get_position(self):
+		return self.row, self.col
+
+	def draw(self, win):
+		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+    
+  def __lt__(self, other):
+		return False
